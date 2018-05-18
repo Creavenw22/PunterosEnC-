48325f9e9ca2f137572c884d4131fc1a11a41d87
creavenw22 <francisco.bustamantew22@gmail.com>
2018-05-18 11:58:49 -0300

    Punteros: vectores

--- Vectores/main.c
+++ Vectores/main.c
@@ -7,9 +7,16 @@ int main()
 {
     int numeros[3];
     int *puntero;
+    int aux=333;
+    puntero=numeros;
     numeros[0]=9;
     numeros[1]=99;
     numeros[2]=999;
+
+    printf("\nnumero: %d",numeros);
+    printf("\nnumero: %d",*puntero); //Muestra el contenido de punteros
+    printf("\nnumero: %d",*(puntero+1)); //Muestra el contenido de la siguiente direccion de memoria
+    printf("\nnumero: %d",*(puntero+2));
     mostrar(numeros,3);
     return 0;
 }
