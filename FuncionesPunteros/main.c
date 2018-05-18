876b1a18f4b1a51b4efc04ca076b05f86dd856e1
creavenw22 <francisco.bustamantew22@gmail.com>
2018-05-18 11:14:28 -0300

    Punteros: funciones_v1.01

--- FuncionesPunteros/main.c
+++ FuncionesPunteros/main.c
@@ -3,6 +3,7 @@
 
 void intercambiar(int num1,int num2);
 void intercambiarConPunteros(int *num1,int *num2);
+void PonerEnCero(int *num1);
 
 int main()
 {
@@ -10,6 +11,7 @@ int main()
     int edadDos;
     edadUno=22;
     edadDos=99;
+    PonerEnCero(&edadDos);
     printf("\n Edad 1: %d",edadUno);
     printf("\n Edad 2: %d",edadDos);
     intercambiarConPunteros(&edadUno,&edadDos);
@@ -18,6 +20,11 @@ int main()
 
 
     return 0;
+}
+
+void PonerEnCero(int *num1)
+{
+    *num1=0;
 }
 
 void intercambiar(int num1,int num2)
